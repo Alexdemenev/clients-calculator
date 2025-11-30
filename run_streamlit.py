aac3ac0999a38f5d8944747aa3e8030516167b6f
demenev.a <demenev@1221systems.ru>
2025-11-30 13:09:34 +0300

    Change port

--- run_streamlit.py
+++ run_streamlit.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
             "streamlit",
             "run",
             "app/streamlit_auth.py",
-            "--server.port=8501",
+            "--server.port=8551",
         ]
     )
